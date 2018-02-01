86fab3469653eaf8b749c43987849e908184752f
Jorik de Boer <jboer@localhost.localdomain>
2018-02-01 20:29:27 +0100

    is odd seems to work

--- main.cpp
+++ main.cpp
@@ -2,19 +2,30 @@
 #include <iostream>
 
 /// is_odd main function
-int main(int argc, char* argv[]) 
+int main(int argc, char* argv[])
 {
-  if (argc == 1) return 1;
+  if (argc == 1){
+      std::cout << "No correct input, shame on you!\n";
+      return 1;
+  }
+
   try
   {
-    std::cout << std::boolalpha << (std::stoi(argv[1]) % 2 != 0) << '\n';
+    const int number = std::stoi(argv[1]);
+    const int modulo = number % 2;
+    if(modulo == 1){
+        std::cout << "true\n";
+    }
+    if(modulo == 0){
+        std::cout << "false\n";
+    }
   }
-  catch (const std::invalid_argument&)
-  {
+  catch (const std::invalid_argument&){
+    std::cout << "No correct input, shame on you!\n";
     return 1;
   }
-  catch (const std::out_of_range&)
-  {
+  catch (const std::out_of_range&){
+    std::cout << "No correct input, shame on you!\n";
     return 1;
   }
 }

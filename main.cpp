b585053ba128c5d18e97a6ab52b32dd8cc5e180c
Jorik de Boer <jboer@localhost.localdomain>
2018-02-01 20:41:38 +0100

    for travis and codecov

--- main.cpp
+++ main.cpp
@@ -8,7 +8,6 @@ int main(int argc, char* argv[])
       std::cout << "No correct input, shame on you!\n";
       return 1;
   }
-
   try
   {
     const int number = std::stoi(argv[1]);
@@ -20,12 +19,10 @@ int main(int argc, char* argv[])
         std::cout << "false\n";
     }
   }
-
   catch (const std::invalid_argument&){
     std::cout << "No correct input, shame on you!\n";
     return 1;
   }
-
   catch (const std::out_of_range&){
     std::cout << "No correct input, shame on you!\n";
     return 1;
